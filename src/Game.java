9e2ab27e8501fed5f56273d8596646e0f703bcfd
Luca Fulmor <gfulmor@outlook.com>
2022-03-01 13:05:25 -0500

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -5,18 +5,17 @@ public class Game {
 	String genre;
 	String score;
 	String plot;
-	String retialer;
+	String retailers;
 	String platform;
 	String title;
 	String rating;
-	
-	public static String Command()
-	{
+
+	public static String Command() {
 		return "\tGame (G)\n";
 	}
 
 	public static void ListAttributes() {
-		 System.out.println("\t1 GenreID\n\t2 Title\n\t3 PageViews\n\t4 Platform\n\t5 Score");
+		System.out.println("\t1 GenreID\n\t2 Title\n\t3 PageViews\n\t4 Platform\n\t5 Score");
 	}
-	
+
 }
