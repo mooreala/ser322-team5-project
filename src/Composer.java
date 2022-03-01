53452d236fad553cab64985af82463b2de75392e
chrisalfaro89 <70774870+chrisalfaro89@users.noreply.github.com>
2022-02-28 21:33:35 -0800

    Add files via upload

--- src/Composer.java
+++ src/Composer.java
@@ -1,8 +1,13 @@
+package Project4Files;
+
 public class Composer {
 	String SSN;
 	String GameID;
 	public static String Command() {
 		return "\tComposer (C)\n";
+	}
+	public static void ListAttributes() {
+		 System.out.println("\t1 SSN\n\t2 Game_ID");
 	}
 
 }
--- src/GameSales.java
+++ src/GameSales.java
@@ -1,3 +1,5 @@
+package Project4Files;
+
 public class GameSales {
 	String GameID;
 	String Month;
@@ -6,5 +8,8 @@ public class GameSales {
 	public static String Command()
 	{
 		return "\tGame Sales (GS)\n";
+	}
+	public static void ListAttributes() {
+		 System.out.println("\t1 Game ID\n\t2 Month\n\t3 Year\n\t4 Amount");
 	}
 }
